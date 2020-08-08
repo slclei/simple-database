edba8854076436fe9863e93fbd7432ae0e5046a5
Lei Xu <qsxulei@gmail.com>
2020-08-07 22:57:12 -0600

    part 2
    Frame work completed, with complier and VM. 2 actions, select and insert, could be recognized.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+//enums for success of failure for meta command
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+//enums for success of failure for prepare step (compiler)
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+//statement type, TODO
+typedef enum{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+//statement struct, TODO
+typedef struct{
+    StatementType type;
+} Statement;
+
 //a wrapper structer to record input buffer
 typedef struct {
     char* buffer;
@@ -46,6 +69,52 @@ void close_input_buffer(InputBuffer* input_buffer){
     free(input_buffer);
 }
 
+//wrapper for more commands
+//TODO
+MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
+    //in case of '.exit'
+    if (strcmp(input_buffer->buffer,".exit")==0){
+        exit(EXIT_SUCCESS);
+    }
+    //TODO
+    //in different cases
+    else{
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+//works as SQL Compiler, to parse command
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+    //in case of insert command
+    if (strncmp(input_buffer->buffer,"insert", 6)==0){
+        statement->type=STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    //in case of select command
+    if (strcmp(input_buffer->buffer,"select")==0){
+        statement->type=STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    //return unrecognize otherwise
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+//works as virtal machine, to exe command
+void execute_statment(Statement* statement){
+    //accroding to result from compiler, exe in different manner
+    //TODO with real action
+    switch (statement->type){
+        //TODO
+        case (STATEMENT_INSERT):
+            printf("for insert.\n");
+            break;
+        //TODO
+        case (STATEMENT_SELECT):
+            printf("for select.\n");
+            break;
+    }
+}
 
 //main function, a infinit loop to show prompt, get a input line, and process the line
 int main(int argc, char* argv[]){
@@ -60,15 +129,30 @@ int main(int argc, char* argv[]){
         //get a new input line, and store that in the input buffer
         read_input(input_buffer);
 
-        //exit with input of '.exit'
-        if(strcmp(input_buffer->buffer,".exit")==0) {
-            //free the space allocated
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+        //for input starting with '.', deal with meta command
+        if (input_buffer->buffer[0]=='.'){
+            //process the input
+            switch (do_meta_command(input_buffer)){
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s' \n", input_buffer->buffer);
+                    continue;
+            }
         }
-        //otherwise, process the input line
-        else{
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+
+        Statement statement;
+        //to exe with success compile, or go to next loop if not
+        switch (prepare_statement(input_buffer, &statement)){
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword '%s' \n", input_buffer->buffer);
+                continue;
         }
+
+        //TODO exe actual command
+        execute_statment(&statement);
+        printf("Executed.\n");
     }
 }
